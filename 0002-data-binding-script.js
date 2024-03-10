163c341d4ea64231ba28e251df4a0ef7d6925915
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-03-10 22:30:28 +0300

    Create 0002-data-binding-script.js

---
+++ 0002-data-binding-script.js
@@ -0,0 +1,11 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            lang1: 'Rust',
+            lang2: 'Move',
+            lang3: 'Solidity',
+        };
+    },
+});
+
+app.mount('#webthree');
