d961a81100976eeab290ef43874c87831b7df2bf
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-03-10 22:40:50 +0300

    Create 0003-Vbind-script.js

---
+++ 0003-Vbind-script.js
@@ -0,0 +1,12 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            lang1: 'Rust',
+            lang2: 'Move',
+            lang3: 'Solidity',
+            googleIt: 'https://www.google.com/',
+        };
+    },
+});
+
+app.mount('#webthree');
