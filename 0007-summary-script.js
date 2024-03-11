886e46bc8b3b8251b3c9fe0902811674ffba3cd2
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-03-11 09:24:52 +0300

    Create 0007-summary-script.js

---
+++ 0007-summary-script.js
@@ -0,0 +1,13 @@
+const app = Vue.createApp({
+  data() {
+    return {
+        name: "Mustafa Büyükdereli",
+        iSay: "I'll do my best and always keep it simple!",
+        myPicUrl: "https://media.licdn.com/dms/image/D4D03AQFWZadKgsJIsg/profile-displayphoto-shrink_400_400/0/1705448278916?e=1715817600&v=beta&t=-kVSMnGFFOIpfwghLjMecKfKB3qq9tV7W3r-p79HC1Q",
+    };
+  },
+
+  methods: {},
+});
+
+app.mount("#vuebasics");
