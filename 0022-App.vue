6eb30ae956ae2ee62f81fbd973218c4c9a5a2593
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-03-14 10:22:48 +0300

    Create 0022-App.vue

---
+++ 0022-App.vue
@@ -0,0 +1,29 @@
+<!-- App.vue file -->
+<!-- Let's use Vue CLI to create a Vue project -->
+
+<template>
+  <h3>Team Members</h3>
+  <ul v-for="member in teamMembers" v-bind:key="member.name">
+    <li>{{ member.name }} : {{ member.title }}</li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: "App",
+
+  data() {
+    return {
+      teamMembers: [
+        { name: "Mustafa", title: "Founder" },
+        { name: "Bilge Kutluk", title: "CFO" },
+        { name: "Bumin Kültigin", title: "CTO" },
+        { name: "Attila Kağan", title: "Auditor" },
+      ],
+    };
+  },
+};
+</script>
+
+<style>
+</style>
